5ae38c96d6dab6a1ec1d866205ca6a417275f4cf
isenkodev <germanhuertaalvial690@gmail.com>
2024-05-22 03:53:06 -0400

    2.3 Nav Responsive & Rediseño

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,45 @@
+function BuscadorHtml(event) {
+    event.preventDefault(); // Prevenir que el formulario se envíe de la manera tradicional
+
+    var searchInput = document.getElementById('termino');
+    var searchText = searchInput.value.toLowerCase();
+
+    switch (searchText) {
+        case "inicio":
+            window.location.href = "index.html";
+            break;
+        case "trabajos":
+            window.location.href = "trabajos.html";
+            break;
+        case "trabajadores":
+            window.location.href = "trabajadores.html";
+            break;
+        case "ventas":
+            window.location.href = "ventas.html";
+            break;
+        case "soporte":
+            window.location.href = "soporte.html";
+            break;
+        default:
+            searchInput.setAttribute('placeholder', 'No existe categoria');
+            setTimeout(function() {
+                searchInput.setAttribute('placeholder', 'Buscar por categoria');
+            }, 2000);
+            searchInput.value = ''; // Limpiar el campo de búsqueda
+    }
+}
+
+/*Animacion .mesage para el login y registro*/ 
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
 
 function loguear() {
     let username = document.querySelector(".login-form input[type='email']").value;
     let password = document.querySelector(".login-form input[type='password']").value;
     let error = document.getElementById("error");
-    
+
     if (username === "" || password === "") {
         error.textContent = "Por favor, completa todos los campos";
         setTimeout(function () {
@@ -25,6 +58,7 @@ function loguear() {
     }
 
     if ((username === "alanbrito@gmail.com" && password === "123456789") || (username === "german@gmail.com" && password === "123456789")) {
+        localStorage.setItem('loggedIn', 'true');
         window.location = "index.html";
     } else {
         error.textContent = "Los datos ingresados son incorrectos";
@@ -36,34 +70,3 @@ function loguear() {
         }, 2000);
     }
 }
-
-function buscarYRedirigir(event) {
-    if (event && event.type === 'keypress' && event.key !== 'Enter') return;
-    
-    var searchText = document.querySelector('.searchInput').value.toLowerCase();
-    var searchInput = document.querySelector('.searchInput');
-    
-    switch(searchText) {
-        case "inicio":
-            window.location.href = "index.html";
-            break;
-        case "trabajos":
-            window.location.href = "trabajos.html";
-            break;
-        case "trabajadores":
-            window.location.href = "trabajadores.html";
-            break;
-        case "ventas":
-            window.location.href = "ventas.html";
-            break;
-        case "soporte":
-            window.location.href = "soporte.html";
-            break;
-        default:
-            searchInput.setAttribute('placeholder','No existe la categoría');
-            setTimeout(function() {
-                searchInput.setAttribute('placeholder', 'Buscar por Categoria');
-            }, 2000);
-    }
-}
-
